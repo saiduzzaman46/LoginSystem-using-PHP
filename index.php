08a7e1b76494357733a5355b66107f70ee16c0f7
saiduzzaman46 <saiduzzamansohag0@gmail.com>
2025-03-13 00:41:37 +0600

    Add title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once "include/signup_view.php";
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Signup</title>
     <link rel="stylesheet" href="style.css" />
 
     <script>
